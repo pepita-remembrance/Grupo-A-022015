1b22ba5f47bc744e8ab1e5a831112a80f0dde482
ariel <alvarez.ariel.work@gmail.com>
2015-08-18 20:44:14 -0300

    initial commit

---
+++ client/views/home/home.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('supergol')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home/home.html',
+        controller: 'HomeCtrl',
+        controllerAs: 'vm'
+      });
+  });
