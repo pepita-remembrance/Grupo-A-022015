776a09ca01762639cae5ffd71556249755c1910f
Federico Aloi <federico.aloi@gmail.com>
2015-10-07 02:50:56 -0300

    Added Position to Player

--- server/models/player.js
+++ server/models/player.js
@@ -2,7 +2,15 @@
 
 export default function(sequelize, DataTypes) {
   const Player = sequelize.define('Player', {
-    name: { type: DataTypes.STRING , allowNull: false }
+    name: {
+      type: DataTypes.STRING ,
+      allowNull: false
+    },
+    position: {
+      type: DataTypes.ENUM,
+      values: ['forward', 'midfielder', 'defender', 'goalkeeper'],
+      allowNull: false
+    }
   }, {
     classMethods: {
       associate: models => Player.belongsTo(
--- server/models/player.spec.js
+++ server/models/player.spec.js
@@ -1,27 +1,41 @@
 'use strict';
 
 import 'should-promised';
+import _ from 'lodash';
 
 import { sequelize, RealWorldTeam, Player } from './index.js';
 
 describe('Player', () => {
   describe('create()', () => {
+    let aTeam;
+    let createPlayer;
+
+    beforeEach(() => {
+      return RealWorldTeam.create({ name: 'Argentinos Juniors' }).then(team => {
+        aTeam = team;
+        createPlayer = (attributes) => Player.create(_.extend({ name: 'Sarasa', RealWorldTeamId: aTeam.id, position: 'goalkeeper' }, attributes));
+      });
+    });
+
     it('adds the object to the DB', () => {
-      return RealWorldTeam.create({ name: 'Argentinos Juniors' })
-        .then(team => Player.create({ name: 'Diego Armando Maradona', RealWorldTeamId: team.id }))
+      return Player.create({ name: 'Diego Armando Maradona', position: 'goalkeeper', RealWorldTeamId: aTeam.id })
         .then(player => Player.findOne({ where: { id: player.id }, include: [ RealWorldTeam ] }))
         .then(it => it.get({ plain: true }))
         .should.eventually
-        .have.properties({ name: 'Diego Armando Maradona' })
+        .have.properties({ name: 'Diego Armando Maradona', position: 'goalkeeper' })
         .and.have.propertyByPath('RealWorldTeam', 'name').eql('Argentinos Juniors');
     });
 
     it('requires a name', () => {
-      return Player.create({}).should.be.rejectedWith(/name cannot be null/);
+      return createPlayer({ name: null }).should.be.rejectedWith(/name cannot be null/);
+    });
+
+    it('requires a valid position', () => {
+      return createPlayer({ position: 'developer' }).should.be.rejectedWith(/"developer" is not a valid choice/);
     });
 
     it('belongs to a real world team', () => {
-      return Player.create({ name: 'Sarasa' }).should.be.rejectedWith(/RealWorldTeamId cannot be null/);
+      return createPlayer({ RealWorldTeamId: null }).should.be.rejectedWith(/RealWorldTeamId cannot be null/);
     });
   });
 });
