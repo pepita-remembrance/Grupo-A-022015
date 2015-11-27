2aef00698ca98dcb4eddb18d591e22219d54fa80
Federico Aloi <federico.aloi@gmail.com>
2015-11-27 20:56:49 -0300

    Player's can be persisted

--- src/main/java/unq/dapp/supergol/model/Player.java
+++ src/main/java/unq/dapp/supergol/model/Player.java
@@ -2,10 +2,15 @@ package unq.dapp.supergol.model;
 
 import unq.dapp.supergol.model.repositories.Persistable;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Player extends Persistable {
+  @ManyToOne
   private RealWorldTeam realWorldTeam;
   private Position position;
-  private int id;
+  private String name;
 
   public static Player goalkeeper(RealWorldTeam team) {
     return Player.ofTeam(Position.GOALKEEPER, team);
@@ -27,16 +32,24 @@ public class Player extends Persistable {
     return player;
   }
 
+  public static Player defender(RealWorldTeam team) {
+    return Player.ofTeam(Position.DEFENDER, team);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
   public int scoreFor(Stage stage) {
     return position.scoreFor(this, stage);
   }
 
   public RealWorldTeam getRealWorldTeam() {
     return realWorldTeam;
-  }
-
-  public static Player defender(RealWorldTeam team) {
-    return Player.ofTeam(Position.DEFENDER, team);
   }
 
   public Position getPosition() {
--- src/test/java/unq/dapp/supergol/model/persistence/PersistenceTest.java
+++ src/test/java/unq/dapp/supergol/model/persistence/PersistenceTest.java
@@ -1,17 +1,37 @@
 package unq.dapp.supergol.model.persistence;
 
+import org.junit.Before;
 import org.junit.Test;
+import unq.dapp.supergol.model.Player;
 import unq.dapp.supergol.model.RealWorldTeam;
 
 import static org.junit.Assert.assertEquals;
 
 public class PersistenceTest extends BasePersistenceTest {
+
+  private RealWorldTeam sanLorenzo;
+
+  @Before
+  public void setUp() throws Exception {
+    sanLorenzo = RealWorldTeam.named("San Lorenzo");
+  }
+
   @Test
   public void realWorldTeamsCanBePersisted() {
-    RealWorldTeam sanLorenzo = RealWorldTeam.named("San Lorenzo");
     persist(sanLorenzo);
 
     assertEquals(1l, sanLorenzo.getId());
     assertEquals(sanLorenzo, find(RealWorldTeam.class, 1l));
+  }
+
+  @Test
+  public void playersCanBePersisted() {
+    Player ortigoza = Player.midfielder(sanLorenzo);
+    ortigoza.setName("Nestor Ortigoza");
+
+    persist(ortigoza);
+
+    assertEquals(1l, ortigoza.getId());
+    assertEquals(ortigoza, find(Player.class, 1l));
   }
 }
