153f37c1aa34c063dcc53ab6822941497f456c77
Federico Aloi <federico.aloi@gmail.com>
2015-11-27 16:51:34 -0300

    A Match can compute the score of a Team

--- src/main/java/unq/dapp/supergol/model/Match.java
+++ src/main/java/unq/dapp/supergol/model/Match.java
@@ -1,17 +1,38 @@
 package unq.dapp.supergol.model;
 
 public class Match {
-  private RealWorldTeam homeTeam;
-  private RealWorldTeam awayTeam;
-  public Stage stage;
+  private static final int TIE_SCORE = 1;
+  private static final int WINNER_SCORE = 3;
+  private static final int LOSER_SCORE = 0;
 
-  public static Match versus(RealWorldTeam homeTeam, RealWorldTeam awayTeam) {
+  private Stage stage;
+  private Team homeTeam;
+  private Team awayTeam;
+
+  public static Match versus(Stage stage, Team homeTeam, Team awayTeam) {
     Match match = new Match();
+    match.stage = stage;
     match.homeTeam = homeTeam;
     match.awayTeam = awayTeam;
 
-    match.stage = new Stage();
-
     return match;
+  }
+
+  public int scoreFor(Team team) {
+    if (isATie()) return TIE_SCORE;
+    if (isTheWinner(team)) return WINNER_SCORE;
+    return LOSER_SCORE;
+  }
+
+  private boolean isTheWinner(Team team) {
+    return team.scoreFor(stage) > oppositeTeam(team).scoreFor(stage);
+  }
+
+  private Team oppositeTeam(Team team) {
+    return team == homeTeam ? awayTeam : homeTeam;
+  }
+
+  private boolean isATie() {
+    return homeTeam.scoreFor(stage) == awayTeam.scoreFor(stage);
   }
 }
