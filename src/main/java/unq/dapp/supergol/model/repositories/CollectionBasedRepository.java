89fb5effdebaecebcdccc83708a55caeedab23b2
Federico Aloi <federico.aloi@gmail.com>
2015-11-29 17:47:55 -0300

    Removed filter message from Repository

--- src/main/java/unq/dapp/supergol/model/repositories/CollectionBasedRepository.java
+++ src/main/java/unq/dapp/supergol/model/repositories/CollectionBasedRepository.java
@@ -3,8 +3,6 @@ package unq.dapp.supergol.model.repositories;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class CollectionBasedRepository<T extends Persistable> implements Repository<T> {
   private Collection<T> elements = new ArrayList<T>();
@@ -20,13 +18,7 @@ public class CollectionBasedRepository<T extends Persistable> implements Reposit
   }
 
   @Override
-  public Collection<T> filter(Predicate<T> comparator) {
-    return elements.stream().filter(comparator).collect(Collectors.<T>toList());
-  }
-
-  @Override
-  public Repository<T> add(T element) {
+  public void add(T element) {
     elements.add(element);
-    return this;
   }
 }
--- src/main/java/unq/dapp/supergol/model/repositories/Repository.java
+++ src/main/java/unq/dapp/supergol/model/repositories/Repository.java
@@ -2,14 +2,11 @@ package unq.dapp.supergol.model.repositories;
 
 import java.util.Collection;
 import java.util.Optional;
-import java.util.function.Predicate;
 
 public interface Repository<T extends Persistable> {
   Collection<T> all();
 
   Optional<T> findById(long id);
 
-  Collection<T> filter(Predicate<T> comparator);
-
-  Repository<T> add(T element);
+  void add(T element);
 }
