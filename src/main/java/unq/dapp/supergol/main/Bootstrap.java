e958ddc1ba440e74072a0bbd2030056a111f4f03
Federico Aloi <federico.aloi@gmail.com>
2015-11-29 19:30:29 -0300

    Added players to seed data

--- src/main/java/unq/dapp/supergol/main/Bootstrap.java
+++ src/main/java/unq/dapp/supergol/main/Bootstrap.java
@@ -1,17 +1,16 @@
 package unq.dapp.supergol.main;
 
+import javafx.util.Pair;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 import unq.dapp.supergol.dependencyInjection.DI;
 import unq.dapp.supergol.dependencyInjection.WithProductionDependencies;
-import unq.dapp.supergol.model.League;
-import unq.dapp.supergol.model.Match;
-import unq.dapp.supergol.model.Stage;
-import unq.dapp.supergol.model.Team;
+import unq.dapp.supergol.model.*;
 import unq.dapp.supergol.model.repositories.Repository;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Arrays;
 
 public class Bootstrap implements DI, WithProductionDependencies, TransactionalOps, WithGlobalEntityManager {
 
@@ -21,6 +20,32 @@ public class Bootstrap implements DI, WithProductionDependencies, TransactionalO
 
   public void run() {
     withTransaction(() -> {
+      newTeam("Club Atletico San Lorenzo de Almagro",
+        new Pair<>(Position.GOALKEEPER, "Sebastian Torrico"),
+        new Pair<>(Position.DEFENDER, "Julio Alberto Buffarini"),
+        new Pair<>(Position.DEFENDER, "Mario Yepes"),
+        new Pair<>(Position.DEFENDER, "Emanuel Mas"),
+        new Pair<>(Position.MIDFIELDER, "Leandro Romagnoli"),
+        new Pair<>(Position.MIDFIELDER, "Nestor Ortigoza"),
+        new Pair<>(Position.MIDFIELDER, "Sebastian Blanco"),
+        new Pair<>(Position.MIDFIELDER, "Juan Ignacio Mercier"),
+        new Pair<>(Position.FORWARD, "Mauro Matas"),
+        new Pair<>(Position.FORWARD, "Hector Daniel Villalba"),
+        new Pair<>(Position.FORWARD, "Martin Cauteruccio"));
+
+      newTeam("Club Atletico Huracan",
+        new Pair<>(Position.GOALKEEPER, "Marcos Guillermo Diaz"),
+        new Pair<>(Position.DEFENDER, "Carlos Arano"),
+        new Pair<>(Position.DEFENDER, "Federico Mancinelli"),
+        new Pair<>(Position.DEFENDER, "Martin Nervo"),
+        new Pair<>(Position.MIDFIELDER, "Patricio Toranzo"),
+        new Pair<>(Position.MIDFIELDER, "Edson Puch"),
+        new Pair<>(Position.MIDFIELDER, "Sebastian Blanco"),
+        new Pair<>(Position.MIDFIELDER, "Juan Ignacio Mercier"),
+        new Pair<>(Position.FORWARD, "Mauro Matas"),
+        new Pair<>(Position.FORWARD, "Hector Daniel Villalba"),
+        new Pair<>(Position.FORWARD, "Martin Cauteruccio"));
+
       Repository<League> leagueRepo = getRepository(League.class);
 
       Team villaLuro = Team.named("Los pibes de Villa Luro");
@@ -42,4 +67,20 @@ public class Bootstrap implements DI, WithProductionDependencies, TransactionalO
     });
   }
 
+  @SafeVarargs
+  private final void newTeam(String name, Pair<Position, String>... players) {
+    RealWorldTeam sanLorenzo = RealWorldTeam.named(name);
+
+    Arrays.asList(players)
+      .stream()
+      .map(it -> newPlayer(it.getKey(), it.getValue(), sanLorenzo))
+      .forEach(it -> getRepository(Player.class).add(it));
+  }
+
+  private Player newPlayer(Position position, String name, RealWorldTeam team) {
+    Player player = Player.ofTeam(position, team);
+    player.setName(name);
+
+    return player;
+  }
 }
