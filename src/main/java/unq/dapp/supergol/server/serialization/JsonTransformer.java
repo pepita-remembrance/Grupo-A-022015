26b472935bbbc43ea276a299bd7c23c1cdeee98f
Federico Aloi <federico.aloi@gmail.com>
2015-11-29 21:09:28 -0300

    Dates are serialized as ISO 8601

--- src/main/java/unq/dapp/supergol/server/serialization/JsonTransformer.java
+++ src/main/java/unq/dapp/supergol/server/serialization/JsonTransformer.java
@@ -1,11 +1,14 @@
 package unq.dapp.supergol.server.serialization;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import spark.ResponseTransformer;
 
+import java.time.LocalDate;
+
 public class JsonTransformer implements ResponseTransformer {
+
   private Gson gson = new GsonBuilder()
+    .registerTypeAdapter(LocalDate.class, new LocalDateJsonSerializer())
     .setExclusionStrategies(new AnnotationExclusionStrategy())
     .create();
 
