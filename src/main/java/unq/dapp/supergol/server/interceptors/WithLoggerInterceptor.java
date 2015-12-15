330eec93e4a79c0a3e4e1b7aed0cde602e01f574
Federico Aloi <federico.aloi@gmail.com>
2015-12-15 18:08:42 -0300

    Added request logging

--- src/main/java/unq/dapp/supergol/server/interceptors/WithLoggerInterceptor.java
+++ src/main/java/unq/dapp/supergol/server/interceptors/WithLoggerInterceptor.java
@@ -2,11 +2,24 @@ package unq.dapp.supergol.server.interceptors;
 
 
 import spark.Filter;
+import spark.Request;
+
+import java.time.LocalDateTime;
 
 public interface WithLoggerInterceptor  {
   default Filter loggingInterceptor(){
     return (request,response)->{
-
+      new Logger().info(request);
     };
+  }
+}
+
+class Logger {
+  void info(Request request) {
+    System.out.println(String.format("%s - %s: %s", timestamp(), request.requestMethod(), request.url()));
+  }
+
+  private LocalDateTime timestamp() {
+    return LocalDateTime.now();
   }
 }
